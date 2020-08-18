0bc6deea7b1fb72a2d7bcb63b7e2a2af35827d6c
Yun Seob Lee <fhzotxldj@gmail.com>
2020-08-18 09:23:18 -0400

    initial commit

---
+++ src/main/java/com/example/boneage/BoneageApplication.java
@@ -0,0 +1,13 @@
+package com.example.boneage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoneageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoneageApplication.class, args);
+	}
+
+}
